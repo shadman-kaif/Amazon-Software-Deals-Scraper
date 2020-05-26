2d215cef176e39f7b1a7054ccb55cd038367a404
Shadman Kaif <kaifshadman@gmail.com>
2020-05-26 17:45:32 -0400

    Final commit

--- spiders/deal.py
+++ spiders/deal.py
@@ -3,7 +3,7 @@ import scrapy
 
 class DealSpider(scrapy.Spider):
     name = 'deal'
-    start_urls = ['https://www.amazon.ca/gp/goldbox?ref_=nav_cs_gb&page=1']
+    start_urls = ['https://www.amazon.ca/Best-Sellers-Software/zgbs/software/ref=zg_bs_software_home_all?pf_rd_p=d8978078-43b5-4da2-9cfc-1b5da282df98&pf_rd_s=center-4&pf_rd_t=2101&pf_rd_i=home&pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_r=BMFGPEZYJ6K2YB85C5ZF&pf_rd_r=BMFGPEZYJ6K2YB85C5ZF&pf_rd_p=d8978078-43b5-4da2-9cfc-1b5da282df98&page=1']
     page_number = 2
 
     def parse(self, response):
@@ -13,8 +13,8 @@ class DealSpider(scrapy.Spider):
         price = response.css('span.p13n-sc-price::text').extract()
 
         # Manipulating price extraction in order to get max and min price
-        max_price = del price[0::2]
-        min_price = del price[1::2]
+        #max_price = [v for i, v in enumerate(price) if i % 2 == 1]
+        #min_price = [v for i, v in enumerate(price) if i % 2 == 0]
 
         # Fixing the div extraction for title
         title = map(lambda s: s.strip(), title)
@@ -22,24 +22,35 @@ class DealSpider(scrapy.Spider):
         del title[0:3]
         title = title[:-4]
 
+        if len(title) == 0:
+            title = [""]
+
+        if len(price) == 0:
+            price = [""]
+
+        if len(review) == 0:
+            review = [""]  
+
+        if len(stars) == 0:
+            stars = [""]  
+
         # Output 
-        for item in zip(title, stars, review, min_price, max_price):
+        for item in zip(title, stars, review, price):
             # Create a dictionary to store the scraped info
             scraped_info = {
                 'Title' : item[0],
                 'Stars' : item[1],
                 'Review' : item[2],
-                'Min Price' : item[3],
-                'Max Price' : item[4]
+                'Price' : item[3]
             }
 
             # Yield or give the scraped info to scrapy
             yield scraped_info
 
             # Next search results
-            next_page = 'https://www.amazon.ca/gp/goldbox?ref_=nav_cs_gb&page=' + str(DealSpider.page_number)
+            next_page = 'https://www.amazon.ca/Best-Sellers-Software/zgbs/software/ref=zg_bs_software_home_all?pf_rd_p=d8978078-43b5-4da2-9cfc-1b5da282df98&pf_rd_s=center-4&pf_rd_t=2101&pf_rd_i=home&pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_r=BMFGPEZYJ6K2YB85C5ZF&pf_rd_r=BMFGPEZYJ6K2YB85C5ZF&pf_rd_p=d8978078-43b5-4da2-9cfc-1b5da282df98&page=' + str(DealSpider.page_number)
 
-            if DealSpider.page_number <= 20:
+            if DealSpider.page_number <= 3:
                 # Increment page number
                 DealSpider.page_number += 1
                 # Recursive callback
